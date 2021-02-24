8aa1ae4f47cf564b9aa79c54f0928d3d220db4f6
Horizon371 <45963302+Horizon371@users.noreply.github.com>
2021-02-24 21:49:46 +0200

    Started UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,13 +9,17 @@ import {  MatButtonModule } from '@angular/material/button';
 import {  MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DatabasePageComponent } from './components/database-page/database-page.component';
-import { GeneFrequencyPageComponent } from './components/gene-frequency-page/gene-frequency-page.component'
+import { GeneFrequencyPageComponent } from './components/gene-frequency-page/gene-frequency-page.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component'
+import { MatDialog } from '@angular/material/dialog';
+import { UploadFileService } from './services/upload-file/upload-file-service.service';
 @NgModule({
   declarations: [
     AppComponent,
     ApplicationNavbarComponent,
     DatabasePageComponent,
     GeneFrequencyPageComponent,
+    UploadFileComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,7 @@ import { GeneFrequencyPageComponent } from './components/gene-frequency-page/gen
     MatButtonModule,
     MatInputModule,
   ],
-  providers: [],
+  providers: [MatDialog,UploadFileService, OverlayModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/database-page/database-page.component.ts
+++ src/app/components/database-page/database-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+// import { UploadFileComponent } from '../upload-file/upload-file.component'
 
 @Component({
+  // providers:  [UploadFileComponent],
   selector: 'app-database-page',
   templateUrl: './database-page.component.html',
   styleUrls: ['./database-page.component.css']
 })
 export class DatabasePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
   }
 
+  openDialog(): void {
+    // const dialogRef = this.dialog.open(UploadFileComponent, {})
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    // });
+  }
+
+
 }
+
